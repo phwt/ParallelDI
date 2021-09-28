6fe384ddcec4185c1178769d6823c3430bf61395
phwt.smwt <phwt.smwt@gmail.com>
2021-09-28 18:45:43 +0700

    chore: remove boilerplate

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -14,11 +14,6 @@ namespace ParallelDI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly GuidService _guidService;
 
@@ -26,19 +21,6 @@ namespace ParallelDI.Controllers
         {
             _logger = logger;
             _guidService = guidService;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
 
         [HttpPost]
